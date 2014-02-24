a61516e91d420f4b16b97f30efd2b94cba236e6f
Greg Nietsky <gregory@distrotech.co.za>
2014-02-24 19:47:57 +0200

    Adds WTF platform support for the AArch64 architecture.
    Patch is based on WebKit-gtk patch done by Riku Voipio, and was
    cherry-picked and tested by Marcin Juszkiewicz.
    
    Task-number: QTBUG-35442

--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h
+++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h
@@ -232,6 +232,14 @@
 #define WTF_CPU_X86_64 1
 #endif
 
+/* CPU(AARCH64) - AArch64 */
+#if defined(__aarch64__)
+#define WTF_CPU_AARCH64 1
+#if defined(__AARCH64EB__)
+#define WTF_CPU_BIG_ENDIAN 1
+#endif
+#endif
+
 /* CPU(ARM) - ARM, any version*/
 #if   defined(arm) \
     || defined(__arm__) \
@@ -998,7 +1006,8 @@
     || CPU(ALPHA) \
     || CPU(SPARC64) \
     || CPU(S390X) \
-    || CPU(PPC64)
+    || CPU(PPC64) \
+    || CPU(AARCH64)
 #define WTF_USE_JSVALUE64 1
 #else
 #define WTF_USE_JSVALUE32_64 1
